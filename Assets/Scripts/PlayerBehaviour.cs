783d9d2f2f9e6a7aec41e29247ad60e7e3b85ed7
Azathoth <wandererbetweenplanes@protonmail.com>
2020-09-16 12:06:08 +0200

    Added basis for platformer

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       inp 
+    }
+}
